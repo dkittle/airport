97f6a3b71131d7fd5308dd3dd0071e7e2e9c55c0
dkittle <don@kittle.ca>
2017-07-12 22:18:28 -0400

    moving http server binding parameters to config file

--- src/main/scala/airport/AirportMain.scala
+++ src/main/scala/airport/AirportMain.scala
@@ -3,14 +3,18 @@ package airport
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 
 object AirportMain extends App {
+
   implicit val system = ActorSystem("webserver-actor-system")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
+
+  val config = ConfigFactory.load()
   val server = new WebServer(system)
-  val supervisorActor =
-    system.actorOf(AirportSupervisorActor.props, "airportSupervisorActor")
+  val supervisorActor = system.actorOf(AirportSupervisorActor.props, "airportSupervisorActor")
   supervisorActor ! AirportSupervisorActor.InitializeAirport
-  val bindingFuture = Http().bindAndHandle(server.routes, "localhost", 9000)
+  val bindingFuture =
+    Http().bindAndHandle(server.routes, config.getString("http.interface"), config.getInt("http.port"))
 }
