27f6bf136693a3d1243ca5c8de2f624dfa7fbe25
dkittlel1 <dkittle@loyalty.com>
2017-07-20 10:15:30 -0400

    added tests to make sure airplane releases runway

--- src/test/scala/airport/AirplaneActorSpec.scala
+++ src/test/scala/airport/AirplaneActorSpec.scala
@@ -1,20 +1,17 @@
 package airport
 
 import airport.ActivityDetails._
-import airport.TowerActor.{RequestLanding, RequestTakeoff}
+import airport.AirplaneActor.{LandingClearance, TakeoffClearance}
+import airport.TowerActor.{RelinquishRunway, RequestLanding, RequestTakeoff}
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
-import org.scalatest.{
-  BeforeAndAfterAll,
-  FeatureSpecLike,
-  GivenWhenThen,
-  Matchers
-}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, FeatureSpecLike, GivenWhenThen, Matchers}
 
 import scala.concurrent.duration._
 
 class AirplaneActorSpec
-    extends TestKit(ActorSystem("test-system"))
+    extends TestKit(ActorSystem("test-system", ConfigFactory.parseString("akka.loglevel = OFF")))
     with ImplicitSender
     with FeatureSpecLike
     with GivenWhenThen
@@ -28,26 +25,40 @@ class AirplaneActorSpec
   }
 
   feature("Test the airplane actor") {
-    scenario(
-      "Expect to receive a landing request when it's time for an airplane to land") {
+    scenario("Expect to receive a landing request when it's time for an airplane to land") {
       Given("an aircraft actor")
-      val airplane = TestActorRef(
-        AirplaneActor.props(self, ActivityDetails("123", Noop, 0, 0)))
+      val airplane = TestActorRef(AirplaneActor.props(self, ActivityDetails("123", Noop, 0, 0)))
       When("a Land message is sent")
       airplane ! Land
       Then("the tower should receive a landing request")
       expectMsg(timeout, RequestLanding)
       airplane.stop()
     }
-    scenario(
-      "Expect to receive a takeoff request when it's time for an airplane to takeoff") {
+    scenario("Expect to receive a takeoff request when it's time for an airplane to takeoff") {
       Given("an aircraft actor")
-      val airplane = TestActorRef(
-        AirplaneActor.props(self, ActivityDetails("123", Noop, 0, 0)))
+      val airplane = TestActorRef(AirplaneActor.props(self, ActivityDetails("123", Noop, 0, 0)))
       When("a Takeoff message is sent")
       airplane ! Takeoff
       Then("the tower should receive a takeoff request")
       expectMsg(timeout, RequestTakeoff)
+      airplane.stop()
+    }
+    scenario("Expect to receive a relinquish runway when airplane is done landing") {
+      Given("an aircraft actor")
+      val airplane = TestActorRef(AirplaneActor.props(self, ActivityDetails("123", Noop, 0, 0)))
+      When("a LandingClearance message is sent")
+      airplane ! LandingClearance
+      Then("the tower should receive a relinquish runway")
+      expectMsg(timeout, RelinquishRunway)
+      airplane.stop()
+    }
+    scenario("Expect to receive a relinquish runway when airplane is done taking off") {
+      Given("an aircraft actor")
+      val airplane = TestActorRef(AirplaneActor.props(self, ActivityDetails("123", Noop, 0, 0)))
+      When("a TakeoffClearance message is sent")
+      airplane ! TakeoffClearance
+      Then("the tower should receive a relinquish runway")
+      expectMsg(timeout, RelinquishRunway)
       airplane.stop()
     }
   }
