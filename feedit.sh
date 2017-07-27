c06c8a7b0492b2f91f7614b30bf912d0639c8ef9
dkittlel1 <dkittle@loyalty.com>
2017-07-27 07:49:46 -0400

    a script to feed flights to the app

---
+++ feedit.sh
@@ -0,0 +1,16 @@
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS123","activity":"Takeoff","duration":45,"delay":63}'
+
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS788","activity":"Takeoff","duration":32,"delay":75}'
+
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS456","activity":"Land","duration":105,"delay":78}'
+
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS321","activity":"Takeoff","duration":45,"delay":147}'
+
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS993","activity":"Takeoff","duration":37,"delay":221}'
+
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS623","activity":"Takeoff","duration":37,"delay":244}'
+
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS898","activity":"Land","duration":55,"delay":302}'
+
+curl -i -X POST -H "Content-Type: application/json" http://localhost:9000/airplane -d '{"flight":"IS488","activity":"Takeoff","duration":32,"delay":46}'
+
